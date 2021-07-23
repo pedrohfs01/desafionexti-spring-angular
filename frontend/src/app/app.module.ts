e9e9d208fed96368061f6ed0789cd791023815cc
pedrohfs01 <pedexg@gmail.com>
2021-07-23 13:57:08 -0300

    Adicionando as páginas de cliente/produto/pedido

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuModule } from 'primeng/menu';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { ClientesComponent } from './clientes/clientes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProdutosComponent,
+    PedidosComponent,
+    ClientesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule,
+    MenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/componentes/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  items: MenuItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Clientes', icon: 'pi pi-fw pi-bars', routerLink: ['/clientes'] },
+      { label: 'Produtos', icon: 'pi pi-fw pi-plus', routerLink: ['/produtos'] },
+      { label: 'Pedidos', icon: 'pi pi-fw pi-bars', routerLink: ['/pedidos'] },
+    ]
+  }
+
+}
